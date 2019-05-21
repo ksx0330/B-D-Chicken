ef7488c79b2a4fc042b6db58b7dbc87b6d017a7a
ksx0330 <ksx0330@gmail.com>
2019-05-21 11:33:16 +0900

    navbar update

--- pages/navbar.php
+++ pages/navbar.php
@@ -6,6 +6,31 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <h4 class="mx-2 my-0" style="padding-left: 2rem; padding-right: 2rem;"> <a href="#">메뉴</a></h4>
+            </li>
+            <li class="nav-item">
+                <h4 class="mx-2 my-0"> | </h4>
+            </li>
+            <li class="nav-item">
+                <h4 class="mx-2 my-0" style="padding-left: 2rem; padding-right: 2rem;"> <a href="#">배달조회</a> </h4>
+            </li>
+            <li class="nav-item">
+                <h4 class="mx-2 my-0"> | </h4>
+            </li>
+            <li class="nav-item">
+                <h4 class="mx-2 my-0" style="padding-left: 2rem; padding-right: 2rem;"> <a href="#">커뮤니티</a> </h4>
+            </li>
+            <li class="nav-item">
+                <h4 class="mx-2 my-0"> | </h4>
+            </li>
+            <li class="nav-item">
+                <h4 class="mx-2 my-0" style="padding-left: 2rem; padding-right: 2rem;"> <a href="#">장바구니</a> </h4>
+            </li>
+        </ul>
+
+
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
             <input type="text" class="form-control" placeholder="검색" style="display: inline-block; width: auto;"/>
