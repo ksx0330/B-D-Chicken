fa8ad35fdbb1137c0ace571060c6d7179da0fe9f
Ryu JiHoon <dbwglns123@gmail.com>
2019-05-30 10:09:55 +0900

    add chicken details upload feature with some assets
    also add assets grill.png, airfry.png

---
+++ pages/chicken/upload_action.php
@@ -0,0 +1,45 @@
+<?php
+
+// FILE UPLOAD
+$target_dir = "../../assets/images/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$isUploadSuccess = True;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+if (file_exists($target_file)) {
+  // File already exists;
+  $isUploadSuccess = False;
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      $isUploadSuccess = True;
+    } else {
+      $isUploadSuccess = False;
+    }
+}
+
+// DB INFO UPLOAD
+$con = mysqli_connect("220.68.91.224", "chicken", "passwd", "chicken") or die("ERROR" . mysqli_error($con));
+mysqli_query($con, "set session character_set_connection=utf8;");
+mysqli_query($con, "set session character_set_results=utf8;");
+mysqli_query($con, "set session character_set_client=utf8;");
+
+$target_file = mysqli_real_escape_string($con, $target_file);
+$title = mysqli_real_escape_string($con, $_POST['title']);
+$price = mysqli_real_escape_string($con, $_POST['price']);
+$context = mysqli_real_escape_string($con, $_POST['context']);
+
+$target_file = substr($target_file, 4);
+$sql1 = "INSERT INTO items(title, price, context, rec) VALUES('$title', '$price', '$context', 0)";
+$sql2 = "INSERT INTO item_images(itemId, url) SELECT ID, '$target_file' FROM items WHERE title = '$title'";
+
+mysqli_query($con, $sql1);
+mysqli_query($con, $sql2);
+
+$url = './index.php';
+?>
+<script>
+  alert("<?php
+   if($isUploadSuccess) { echo "Info Uploaded"; } else { echo "Info not uploaded"; }
+  ?>");
+  location.replace("<?php echo $url ?>");
+</script>
