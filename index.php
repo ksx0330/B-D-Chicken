b77148638c9c9403898d17db65fd6d80191ec311
ksx0330 <ksx0330@gmail.com>
2019-06-04 01:05:22 +0900

    footer update

--- index.php
+++ index.php
@@ -251,7 +251,7 @@ mysqli_query($con, "set session character_set_client=utf8;");
   <!-- Footer -->
   <footer class="py-5 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+      <p class="m-0 text-center text-white">Copyright &copy; B&D Chicken (LTaeng Venture)</p>
     </div>
     <!-- /.container -->
   </footer>
