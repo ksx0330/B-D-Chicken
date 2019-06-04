958461525849c73360ad5cafa88bca4b18859b14
YunGoon <dsyun96@naver.com>
2019-06-04 23:55:05 +0900

    pwfind done

--- pages/user/edit_action.php
+++ pages/user/edit_action.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-include "/var/www/html/WebProgramming/sql/connection/dbconnect.php";
+//include "/var/www/html/WebProgramming/sql/connection/dbconnect.php";
+include "../../sql/connection/dbconnect.php";
+include_once '../../lib/encrypt.php';
 
 if (!isset($_SESSION['usr_id'])) {
   echo '<script>
@@ -13,21 +15,39 @@ mysqli_query($con, "set session character_set_connection=utf8;");
 mysqli_query($con, "set session character_set_results=utf8;");
 mysqli_query($con, "set session character_set_client=utf8;");
 
-$user_sql = "SELECT `email`, `tel`, `address`, `point` FROM `user` WHERE `userId`=" . $_SESSION['usr_id'];
+$user_sql = "SELECT `email`, `tel`, `address`, `point`, `password` FROM `user` WHERE `userId`=" . $_SESSION['usr_id'];
 $result = mysqli_query($con, $user_sql);
 while ($row = mysqli_fetch_assoc($result)) {
     $email = $row['email'];
     $p = $row['tel'];
     $address = $row['address'];
     $point = $row['point'];
+    $pw = $row['password'];
 }
 
+$password = hash('sha256', mysqli_real_escape_string($con, $_POST["user_pw"]) . "LTaeng01100");
+
+if ($password !== $pw) {
+  echo $password . "<br>";
+  echo $pw . "<br>";
+  echo "비밀번호가 일치하지 않습니다.";
+  exit();
+}
 
 $name = mysqli_real_escape_string($con, $_POST["user_name"]);
 $addr = mysqli_real_escape_string($con, $_POST["user_addr"]);
 $tel = mysqli_real_escape_string($con, str_replace('-', '', $_POST["user_tel"]));
+$changed_pw = hash('sha256', mysqli_real_escape_string($con, $_POST["user_pwch"]) . "LTaeng01100");
+$changed_pw2 = hash('sha256', mysqli_real_escape_string($con, $_POST["user_pwch2"]) . "LTaeng01100");
 
-$sql = "UPDATE `user` SET `name`='$name', `address`='$addr', `tel`='$tel' WHERE `userId`=" . $_SESSION['usr_id'];
+if ($changed_pw !== $changed_pw2) {
+  echo "재입력한 비밀번호가 일치하지 않습니다.";
+  exit();
+}
+
+$sql = "UPDATE `user` SET `name`='$name', `address`='$addr', `tel`='$tel'";
+if ($changed_pw !== hash('sha256', "LTaeng01100")) $sql .= ", `password`='$changed_pw' WHERE `userId`=" . $_SESSION['usr_id'];
+
 mysqli_query($con, "set session character_set_connection=utf8;");
 mysqli_query($con, "set session character_set_results=utf8;");
 mysqli_query($con, "set session character_set_client=utf8;");
