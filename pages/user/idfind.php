b0152792188662281d101377500aa3b2375ee5e2
YunGoon <dsyun96@naver.com>
2019-06-05 14:58:37 +0900

    local 작업

--- pages/user/idfind.php
+++ pages/user/idfind.php
@@ -1,5 +1,5 @@
 <?php
-include "/var/www/html/WebProgramming/sql/connection/dbconnect.php";
+include "C:/Bitnami/wampstack-7.1.27-0/apache2/htdocs/B-D-Chicken/sql/connection/dbconnect.php";
 
 ?>
 <!DOCTYPE html>
