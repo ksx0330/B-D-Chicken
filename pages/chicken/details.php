8f5081b509aea9ee1c154340fa78c7278a663eb7
YunGoon <dsyun96@naver.com>
2019-06-05 13:40:47 +0900

    edit issue 해결

--- pages/chicken/details.php
+++ pages/chicken/details.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include "C:/Bitnami/wampstack-7.1.27-0/apache2/htdocs/B-D-Chicken/sql/connection/dbconnect.php";
+include "/var/www/html/WebProgramming/sql/connection/dbconnect.php";
 
 mysqli_query($con, "set session character_set_connection=utf8;");
 mysqli_query($con, "set session character_set_results=utf8;");
--- pages/chicken/payment_action.php
+++ pages/chicken/payment_action.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include "C:/Bitnami/wampstack-7.1.27-0/apache2/htdocs/B-D-Chicken/sql/connection/dbconnect.php";
+include "/var/www/html/WebProgramming/sql/connection/dbconnect.php";
 
 if (!isset($_SESSION['usr_id'])) {
     echo '<script>
